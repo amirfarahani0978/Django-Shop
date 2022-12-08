e8a5f83be56318b8754aa92996e3fd8497054e9a
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-09 00:37:34 +0330

    complete offer table

--- store/product/models.py
+++ store/product/models.py
@@ -13,6 +13,13 @@ class Product(models.Model):
     status_available = models.BooleanField()
     quantity = models.PositiveIntegerField() 
     category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    history_price = models.JSONField()
+    rate = models.PositiveIntegerField()
 
 class Offer(models.Model):
-    pass
+    expire_time = models.DateField()
+    min_price = models.PositiveIntegerField()
+    max_price = models.PositiveIntegerField()
+    percent = models.PositiveIntegerField()
+    offer_code = models.CharField(max_length=100)
+    is_available = models.BooleanField()
