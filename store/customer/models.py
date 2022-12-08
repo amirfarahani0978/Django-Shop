ce8427ae8ca409f22f7eaa52fc2f9bc4655e1a84
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-08 15:17:19 +0330

    add customer model

--- store/customer/models.py
+++ store/customer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    GENDER_CHOICE = (('male', 'MALE'), ('female', 'FEMALE'))
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    image = models.ImageField()
+    postal_code = models.IntegerField()
+    gender = models.CharField(max_length=5, choices=GENDER_CHOICE)
+    def __str__(self):
+        return self.firstname
