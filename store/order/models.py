6a81f46a1406f1981889d7a428339db8dda55bbc
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-09 01:01:24 +0330

    add order and ordre_item to models

--- store/order/models.py
+++ store/order/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from product.models import Offer
 # Create your models here.
+ 
+class Order(models.Model):
+    state = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    time = models.DateField()
+    total_price = models.PositiveIntegerField()
+    offer_id = models.OneToOneField(Offer , on_delete=models.CASCADE, related_name='order')
+
+
+class Order_item(models.Model):
+    quantity = models.PositiveIntegerField()
