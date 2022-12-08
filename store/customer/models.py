8f5d8eb14307f2f35d741ca1a0a2dc69f9dfc67c
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-08 18:23:16 +0330

    add customer model

--- store/customer/models.py
+++ store/customer/models.py
@@ -7,8 +7,9 @@ class Customer(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=50)
     birth_date = models.DateField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='')
     postal_code = models.IntegerField()
-    gender = models.CharField(max_length=5, choices=GENDER_CHOICE)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICE)
+    history_buy = models.JSONField()
     def __str__(self):
         return self.firstname
