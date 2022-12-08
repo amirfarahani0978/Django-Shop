1709d495edaaf7f2801124511e4c2c04a5762294
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-08 15:24:58 +0330

    add product model

--- store/product/models.py
+++ store/product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=40)
+    image = models.ImageField()
+    price = models.PositiveIntegerField()
+    count_buying = models.PositiveIntegerField()
+    status_available = models.BooleanField()
+    quantity = models.PositiveIntegerField() 
