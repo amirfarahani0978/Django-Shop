b862a7d2d05e746a62dfe8a7015a3e9cdb5e46f5
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-09 01:13:04 +0330

    add product feature to models

--- store/product/models.py
+++ store/product/models.py
@@ -5,6 +5,11 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
 
 
+class ProductFeature(models.Model):
+    amount = models.PositiveIntegerField()
+    descript = models.CharField(max_length=200)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=40)
     image = models.ImageField()
@@ -15,6 +20,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category , on_delete=models.CASCADE)
     history_price = models.JSONField()
     rate = models.PositiveIntegerField()
+    product_feature = models.OneToOneField(ProductFeature, on_delete=models.CASCADE)
+
 
 class Offer(models.Model):
     expire_time = models.DateField()
@@ -23,3 +30,4 @@ class Offer(models.Model):
     percent = models.PositiveIntegerField()
     offer_code = models.CharField(max_length=100)
     is_available = models.BooleanField()
+
