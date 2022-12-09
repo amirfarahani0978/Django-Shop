710307c2afa0fbb15d4afcf92dcb0d9253058561
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-09 09:17:57 +0330

    add null for order models and add str method

--- store/order/models.py
+++ store/order/models.py
@@ -4,11 +4,15 @@ from product.models import Offer
  
 class Order(models.Model):
     state = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=100 , null=True)
     time = models.DateField()
     total_price = models.PositiveIntegerField()
-    offer_id = models.OneToOneField(Offer , on_delete=models.CASCADE, related_name='order')
+    offer_id = models.OneToOneField(Offer , on_delete=models.CASCADE, related_name='order' , null=True)
+    def __str__(self) -> str:
+        return f"{self.time} + {self.total_price}"
 
 
 class Order_item(models.Model):
     quantity = models.PositiveIntegerField()
+    def __str__(self) -> str:
+        return self.quantity
