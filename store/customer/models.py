562211f1d7f5657deca2f39752f65a64a01f1185
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-09 09:12:49 +0330

    add relation to customer and user

--- store/customer/models.py
+++ store/customer/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-
+from user.models import User
 # Create your models here.
 
 class Customer(models.Model):
     GENDER_CHOICE = (('male', 'MALE'), ('female', 'FEMALE'))
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=50)
-    birth_date = models.DateField()
-    image = models.ImageField(upload_to='')
+    birth_date = models.DateField(null=True)
+    image = models.ImageField(upload_to='' , null=True)
     postal_code = models.IntegerField()
-    gender = models.CharField(max_length=6, choices=GENDER_CHOICE)
-    history_buy = models.JSONField()
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICE , null=True)
+    history_buy = models.JSONField(null=True)
+    user_id = models.OneToOneField(User , on_delete=models.CASCADE)
     def __str__(self):
         return self.firstname
