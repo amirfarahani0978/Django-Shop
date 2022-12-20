a1d9a4043ea0abc734fbab86a0b0a018b7107351
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-20 14:00:37 +0330

    add save mehtod for admin panel

--- store/account/admin.py
+++ store/account/admin.py
@@ -2,14 +2,25 @@ from django.contrib import admin
 from django import forms
 from .models import Account
 from django.core.exceptions import ValidationError
+
+
 class AccountCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password' ,widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password' ,widget=forms.PasswordInput)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password', widget=forms.PasswordInput)
+
     class Meta:
         model = Account
-        fields= ('phone_number' , 'lastname', 'firstname')
+        fields = ('phone_number', 'lastname', 'firstname')
+
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
             raise ValidationError('password dont match')
         return cd['password2']
+
+    def save(self, commit: True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password2'])
+        if commit:
+            user.save()
+        return user
