e2c9e93927c678e29593b3558e35b35c528fb6e0
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-20 13:56:23 +0330

    add creation form for admin panel

--- store/account/admin.py
+++ store/account/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
-# Register your models here.
+from django import forms
+from .models import Account
+from django.core.exceptions import ValidationError
+class AccountCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password' ,widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password' ,widget=forms.PasswordInput)
+    class Meta:
+        model = Account
+        fields= ('phone_number' , 'lastname', 'firstname')
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
+            raise ValidationError('password dont match')
+        return cd['password2']
