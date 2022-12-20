763642543f57b3fa701c0c51c87695a4f81e92c6
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-20 13:04:56 +0330

    complete models of account app

--- store/account/models.py
+++ store/account/models.py
@@ -1,32 +1,48 @@
 from django.db import models
 from core.models import BaseModel
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
-class Account(BaseModel):
+
+
+class Account(AbstractBaseUser):
     GENDER_CHOICE = (('male', 'MALE'), ('female', 'FEMALE'))
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=50)
     birth_date = models.DateField(null=True)
-    image = models.ImageField(upload_to='' , null=True)
+    image = models.ImageField(upload_to='', null=True)
     postal_code = models.IntegerField()
-    gender = models.CharField(max_length=6, choices=GENDER_CHOICE , null=True)
-    phone_number = models.IntegerField()
-    email = models.EmailField()
-    password = models.CharField(max_length=100)
-    is_active = models.BooleanField()
-    is_admin = models.BooleanField()
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICE)
+    phone_number = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
     def __str__(self):
-        return self.firstname
+        return self.phone_number
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
 
 class Address(BaseModel):
     address = models.CharField(max_length=200)
-    account_id = models.ForeignKey(Account , on_delete=models.CASCADE)
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+
     def __str__(self) -> str:
         return self.address
 
+
 class Wishing(BaseModel):
-    user_id = models.OneToOneField(Account , on_delete= models.CASCADE)
+    user_id = models.OneToOneField(Account, on_delete=models.CASCADE)
 
 
 class Wishing_item(BaseModel):
     # product_id = models.ForeignKey(Product , on_delete=models.CASCADE)
-    wishing = models.ForeignKey(Wishing , on_delete=models.CASCADE)
+    wishing = models.ForeignKey(Wishing, on_delete=models.CASCADE)
