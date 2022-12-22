b0b45f1d4d5138af55c9ee66026407bf6939238d
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-22 12:34:36 +0330

    add urls for register and creat class for that

---
+++ store/account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RegisterView
+
+
+app_name = 'register'
+urlpatterns = [
+    path('', RegisterView.as_view(), name='register')
+]
--- store/account/views.py
+++ store/account/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.contrib import admin
-
+from django.views import View
 
 # Create your views here.
+
+
+class RegisterView(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
