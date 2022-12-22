b5ff450e4b25bddc5662ed9c5f97568a510e78c8
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-22 14:48:09 +0330

    add post for view in account

--- store/account/views.py
+++ store/account/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from django.contrib import admin
 from django.views import View
 from .forms import RegistrationForm
-
-
+from .models import Account
+from django.contrib import messages
 class RegisterView(View):
     form_class = RegistrationForm
+
     def get(self, request):
         form = self.form_class
-        return render(request , 'account/register.html' , {'form': form})
+        return render(request, 'account/register.html', {'form': form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            Account.objects.create(
+                username=cd['username'], email=cd['email'], password=cd['password'])
+            messages.success(request, "your login successfully!!!")
+            return redirect('home:home')
+        return render(request , 'home:home' , {'form': form})
