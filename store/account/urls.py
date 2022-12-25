db8a180c7398eecf37c37ae540635e9330a6b680
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2022-12-25 22:22:22 +0330

    add url for verify code

--- store/account/urls.py
+++ store/account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView ,LogOutView , LoginView , ProfileView
+from .views import RegisterView ,LogOutView , LoginView , ProfileView ,VerifyCodeView
 
 
 app_name = 'register'
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', RegisterView.as_view(), name='register'),
     path('logout/' , LogOutView.as_view() , name='logout'),
     path('login/' , LoginView.as_view() , name='login'),
-    path('profile/<int:user_id>/' , ProfileView.as_view() , name='profile')
+    path('profile/<int:user_id>/' , ProfileView.as_view() , name='profile'),
+    path('verifycode/' , VerifyCodeView.as_view() , name='verifycode')
 ]
