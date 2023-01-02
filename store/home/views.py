63031b0282b7b116021b81138f269e2c760361a9
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-02 15:41:09 +0330

    fix:have problem for base.html in view home

--- store/home/views.py
+++ store/home/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views import View
-from product.models import Product
-
+from product.models import Product , Category
 
 class Home(View):
     def get(self, request):
         products = Product.objects.filter(status_available=True)
-        return render(request, 'product/product.html', {'products': products})
+        category = Category.objects.all()
+        return render(request, 'base.html', {'products': products})
 
     # def post(self, request):
     #     return render(request, 'base.html')
