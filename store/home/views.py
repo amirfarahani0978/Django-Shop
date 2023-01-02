7ddb068c26da1881415ee391c10aaed11b809061
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-02 15:46:28 +0330

    fix:complete navbar for show category

--- store/home/views.py
+++ store/home/views.py
@@ -5,8 +5,8 @@ from product.models import Product , Category
 class Home(View):
     def get(self, request):
         products = Product.objects.filter(status_available=True)
-        category = Category.objects.all()
-        return render(request, 'base.html', {'products': products})
+        categories = Category.objects.all()
+        return render(request, 'base.html', {'products': products , 'categories':categories})
 
     # def post(self, request):
     #     return render(request, 'base.html')
