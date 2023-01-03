2f7da76ac894b086f4e8c2de212a45a29909724c
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-03 16:38:31 +0330

    feat:add url for remove product from order

--- store/order/urls.py
+++ store/order/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'order'
 urlpatterns=[
     path('cart/' , views.CartView.as_view() , name='cart'),
-    path('cart/add/<int:product_id>/' , views.CartAddView.as_view() ,name='addcart')
+    path('cart/add/<int:product_id>/' , views.CartAddView.as_view() ,name='addcart'),
+    path('order/remove/<int:product_id>/' , views.RemoveCardView.as_view() , name='remove'),
 ]
