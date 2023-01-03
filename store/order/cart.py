c01ff67a0b7986d2bea679002e42de1b80eafa41
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-03 12:01:15 +0330

    feat:add method to cart class and add save method

--- store/order/cart.py
+++ store/order/cart.py
@@ -1,8 +1,21 @@
 CART_SESSION_ID = 'cart'
+
+
 class Cart:
-    def __init__(self , request) -> None:
+    def __init__(self, request) -> None:
         self.session = request.session
         cart = self.session.get(CART_SESSION_ID)
         if not cart:
             cart = self.session[CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product, quantity):
+        product_id = product.id
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': str(
+                quantity), 'price': str(product.price)}
+        self.cart[product_id]['quantity'] += 1
+        self.save()
+
+    def save(self):
+        self.session.modified = True
