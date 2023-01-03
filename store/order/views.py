bde13cbeeea3d5b77b3c07780222134e17bf3b1b
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-03 11:38:22 +0330

    feat:add url for add cart and add view for it

--- store/order/views.py
+++ store/order/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
+
+
 class CartView(View):
     def get(self, request):
-        return render(request , 'order/cart.html')
+        return render(request, 'order/cart.html')
+
+
+class CartAddView(View):
+    pass
