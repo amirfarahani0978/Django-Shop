74df2d24e00c936626c16cd85bd590ddaab87915
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-03 09:28:43 +0330

    fix:add btn for category for show it

--- store/home/views.py
+++ store/home/views.py
@@ -3,9 +3,12 @@ from django.views import View
 from product.models import Product , Category
 
 class Home(View):
-    def get(self, request):
+    def get(self, request , category_slug:None):
         products = Product.objects.filter(status_available=True)
         categories = Category.objects.all()
+        if category_slug:
+            category = Category.objects.get(slug = category_slug)
+            products = products.filter(category = category)
         return render(request, 'base.html', {'products': products , 'categories':categories})
 
     # def post(self, request):
