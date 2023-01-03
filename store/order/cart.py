f14e50a2aea4a76017a65aa7f4501bcbc2b5ceda
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-03 17:06:02 +0330

    feat:add __len__ method for length about my order

--- store/order/cart.py
+++ store/order/cart.py
@@ -22,6 +22,9 @@ class Cart:
             item['total_price'] = int(item['price']) * item['quantity']
             yield item
 
+    def __len__(self):
+        return sum(item['quatity'] for item in self.cart.values())
+
     def add(self, product, quantity):
         product_id = str(product.id)
         if product_id not in self.cart:
@@ -29,11 +32,13 @@ class Cart:
                 'quantity': 0, 'price': str(product.price)}
         self.cart[product_id]['quantity'] += quantity
         self.save()
-    def remove(self , product):
-        product_id =str(product.id)
+
+    def remove(self, product):
+        product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
     def save(self):
         self.session.modified = True
 
