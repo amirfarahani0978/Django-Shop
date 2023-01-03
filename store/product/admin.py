5c5da247e34f61063fd6da62ee28feb98c5d2eb5
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-03 09:56:48 +0330

    fix:admin register for product

--- store/product/admin.py
+++ store/product/admin.py
@@ -3,6 +3,8 @@ from .models import Category ,Comment ,Product ,ProductFeature,Offer
 
 admin.site.register(Category)
 admin.site.register(Comment)
-admin.site.register(Product)
 admin.site.register(ProductFeature)
 admin.site.register(Offer)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    raw_id_fields = ('category')
