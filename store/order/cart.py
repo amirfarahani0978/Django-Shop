e8afa9eb78a05fc35b62a5d3c218bef25eeb32fd
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-03 13:13:46 +0330

    fix:change quantity count first to zero

--- store/order/cart.py
+++ store/order/cart.py
@@ -24,8 +24,7 @@ class Cart:
     def add(self, product, quantity):
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': str(
-                quantity), 'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
         self.cart[product_id]['quantity'] += quantity
         self.save()
 
