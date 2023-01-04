33c9b0f1f038d884b2432e25ec64ea1052058e78
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-04 11:07:37 +0330

    feat:add loginmixin

--- store/order/views.py
+++ store/order/views.py
@@ -1,27 +1,30 @@
-from django.shortcuts import render,get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .cart import Cart
 from product.models import Product
 from .forms import CartAddForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 class CartView(View):
     def get(self, request):
         cart = Cart(request)
-        return render(request, 'order/cart.html' , {'cart':cart})
+        return render(request, 'order/cart.html', {'cart': cart})
 
 
 class CartAddView(View):
-    def post(self, request , product_id):
+    def post(self, request, product_id):
         cart = Cart(request)
-        product = get_object_or_404(Product , id=product_id)
+        product = get_object_or_404(Product, id=product_id)
         form = CartAddForm(request.POST)
         if form.is_valid():
-            cart.add(product , form.cleaned_data['quantity'])
+            cart.add(product, form.cleaned_data['quantity'])
         return redirect('order:cart')
 
 
 class RemoveCardView(View):
-    def get(self, request , product_id):
+    def get(self, request, product_id):
         cart = Cart(request)
-        product = get_object_or_404(Product , id=product_id)
+        product = get_object_or_404(Product, id=product_id)
         cart.remove(product)
         return redirect('order:cart')
