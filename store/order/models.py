21cb92c908011622568c2f981083446303e0fbba
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-04 10:37:18 +0330

    fix :change model of order

--- store/order/models.py
+++ store/order/models.py
@@ -2,18 +2,19 @@ from django.db import models
 from product.models import Offer
 from core.models import BaseModel
 from product.models import Product
-
+from account.models import Account
 
 class Order(BaseModel):
+    user = models.ForeignKey(Account , on_delete=models.CASCADE , related_name='orders')
     state = models.CharField(max_length=100)
+    paid = models.BooleanField(default=False)
     description = models.CharField(max_length=100, null=True)
-    time = models.DateField()
-    total_price = models.PositiveIntegerField()
+    created = models.DateTimeField()
     offer_id = models.OneToOneField(
         Offer, on_delete=models.CASCADE, related_name='order', null=True)
 
     def __str__(self) -> str:
-        return f"{self.time} + {self.total_price}"
+        return f'{self.user} - {self.id}'
 
     def get_totorial_price(self):
         return sum(item.get_cost() for item in self.items.all())
