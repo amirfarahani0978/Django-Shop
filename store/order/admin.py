ebdaa0a9fab825866dfdec61db48f71fb313d689
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-04 10:33:21 +0330

    fix:change fomat for order and orderitem in admin panel

--- store/order/admin.py
+++ store/order/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Offer, Order, OrderItem
 
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+admin.site.register(Offer)
+
+
+class OrderItemAdmin(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'updated',)
+    list_filter = ('paid',)
+    inlines = (OrderItemAdmin)
