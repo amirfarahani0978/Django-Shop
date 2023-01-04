ec652fe1fa2fbac89eca2dde40171484d60e153f
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-04 10:27:37 +0330

    fix :change models of order

--- store/order/models.py
+++ store/order/models.py
@@ -1,23 +1,31 @@
 from django.db import models
 from product.models import Offer
-from core.models import BaseModel 
+from core.models import BaseModel
 from product.models import Product
+
 
 class Order(BaseModel):
     state = models.CharField(max_length=100)
-    description = models.CharField(max_length=100 , null=True)
+    description = models.CharField(max_length=100, null=True)
     time = models.DateField()
     total_price = models.PositiveIntegerField()
-    offer_id = models.OneToOneField(Offer , on_delete=models.CASCADE, related_name='order' , null=True)
+    offer_id = models.OneToOneField(
+        Offer, on_delete=models.CASCADE, related_name='order', null=True)
+
     def __str__(self) -> str:
         return f"{self.time} + {self.total_price}"
 
+    def get_totorial_price(self):
+        return sum(item.get_cost() for item in self.items.all())
+
 
 class OrderItem(BaseModel):
-    order = models.ForeignKey(Order , on_delete=models.CASCADE , related_name='items')
-    product = models.ForeignKey(Product , on_delete=models.CASCADE)
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.IntegerField()
     quantity = models.IntegerField(default=1)
+
     def __str__(self) -> str:
         return self.quantity
 
