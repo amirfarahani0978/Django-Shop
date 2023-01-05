dbffc78947a2149cbf9fd58a5fd4d68ea2b04293
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-06 00:45:44 +0330

    feat:add view for update profile

--- store/account/api_views.py
+++ store/account/api_views.py
@@ -1,7 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import RegisterSerializers
+from .serializers import RegisterSerializers , ProfileSerializer
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .models import Account
+from .permissions import IsOwnerOrReadOnly
 class Register(APIView):
     def post(self , request):
         ser_data = RegisterSerializers(data=request.POST)
@@ -9,3 +12,15 @@ class Register(APIView):
             ser_data.create(ser_data.validated_data)
             return Response(ser_data.data , status=status.HTTP_201_CREATED)
         return Response(ser_data.errors ,status=status.HTTP_400_BAD_REQUEST)
+
+class ProfileUpdateView(APIView):
+    permission_classes =[IsOwnerOrReadOnly,]
+    def put(self, request, pk):
+        question = Account.objects.get(id=pk)
+        self.check_object_permissions(request ,question)
+        srz_data = ProfileSerializer(
+            instance=question, data=request.POST, partial=True)
+        if srz_data.is_valid():
+            srz_data.save()
+            return Response(srz_data.data, status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
