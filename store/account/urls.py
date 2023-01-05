bc41feb9a0ad16c58c82fd83aca6cc37f65fd80c
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-05 19:40:58 +0330

    feat:add url for register url

--- store/account/urls.py
+++ store/account/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import RegisterView ,LogOutView , LoginView , ProfileView
-
+from .api_views import *
 
 app_name = 'register'
 urlpatterns = [
     path('', RegisterView.as_view(), name='register'),
     path('logout/' , LogOutView.as_view() , name='logout'),
     path('login/' , LoginView.as_view() , name='login'),
-    path('profile/<int:user_id>/' , ProfileView.as_view() , name='profile')
+    path('profile/<int:user_id>/' , ProfileView.as_view() , name='profile'),
+    path('api/', Register.as_view() , name='api-register')
 ]
