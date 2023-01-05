c3ce9fa5832ca3742820d029db0e8986fb4e47ca
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-06 00:50:40 +0330

    feat:add url for update profile

--- store/account/api_urls.py
+++ store/account/api_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .api_views import Register
+from .api_views import Register ,ProfileUpdateView
 from rest_framework.authtoken import views as token
 app_name= 'api'
 urlpatterns = [
     path('register/',Register.as_view() , name='register'),
     path('api-token-auth/' , token.obtain_auth_token),
+    path('profile/update/<int:pk>/' , ProfileUpdateView.as_view() , name='update_question'), 
 ]
