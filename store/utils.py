1249df9827265e5633ce7263d72622b6de22540c
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-06 13:19:39 +0330

    feat:add api cod to my setting

--- store/utils.py
+++ store/utils.py
@@ -1,15 +1,15 @@
 from kavenegar import *
-
+from django.conf import settings
 
 def send_otp_code(phone_number, code):
     try:
-        api = KavenegarAPI('56483559532F5741705069372F784A3275476A4F536F4A4472696F4A55734465673670415979764B7A63513D')
+        KavenegarAPI(settings.API_CODE)
         params = {
             'sender': '',
             'receptor': phone_number, 
             'message': f'{code}کد تایید شما ',
         }
-        response = api.sms_send(params)
+        response = settings.API_CODE.sms_send(params)
         print(response)
     except APIException as e:
         print(e)
