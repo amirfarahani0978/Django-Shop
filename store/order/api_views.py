901f677adeb55439c86883c9785fa913f96d6b69
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-06 13:54:33 +0330

    fix:fix the api

--- store/order/api_views.py
+++ store/order/api_views.py
@@ -24,8 +24,9 @@ class OrderApiView(APIView):
 
     def post(self, request):
         account = Account.objects.get(id=request.user.id) 
-        order = Order.objects.create(user=account)
-        srz_data = ItemSerializer(data=request.data, many=True)
+        # orderitem = OrderItem.objects.create(user=account)
+        self.check_object_permissions(request ,account)
+        srz_data = Order(instance = account,data=request.POST, many=True)
         if srz_data.is_valid():
             srz_data.save()
             return Response(srz_data.data, status=status.HTTP_201_CREATED)
