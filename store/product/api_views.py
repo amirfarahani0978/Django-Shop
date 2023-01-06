5ad57f5c510b3f6a313767147a0860ae4078a67b
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-06 12:07:48 +0330

    feat:add category list

---
+++ store/product/api_views.py
@@ -0,0 +1,14 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework import status
+
+
+
+
+class CategoryListView(APIView):
+    def get(self, request):
+        cat = Category.objects.all()
+        ser_data = CategorySerializer(instance=cat, many=True).data
+        return Response(ser_data, status=status.HTTP_200_OK)
