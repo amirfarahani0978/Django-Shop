e16754167be872a6c22ed0f5a78df86471e0e441
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-06 13:09:03 +0330

    feat:add and fix order to order

--- store/order/api_views.py
+++ store/order/api_views.py
@@ -5,10 +5,7 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-import json
-
-import requests
-
+from rest_framework import status
 from account.models import Account
 from .models import Order, OrderItem
 from .serializers import OrderSerializer, OrderItemsSerializer, ItemSerializer
@@ -31,55 +28,10 @@ class OrderApiView(APIView):
         return Response(content)
 
     def post(self, request):
-        user = request.user
-        account = Account.objects.get(user=user)
-        if account.default_address is not None:
-            order = Order.objects.create(
-                costumer=account, address_id=account.default_address.id)
-            check = ItemSerializer(data=request.data, many=True)
-            if check.is_valid():
-                for i in request.data:
-                    i["order"] = order.id
-                    item = Product.objects.get(
-                        name=i["name"].replace("_", " "))
-                    i["item"] = item.id
-                    i["quantity"] = i["count"]
-                    del i["name"]
-                    del i["price"]
-                    del i["count"]
-                    order_f = OrderItemsSerializer(data=i)
-                    if order_f.is_valid():
-                        order_f.save()
-                    else:
-                        raise ProcessLookupError
-                order.save()
-                resp = requests.post("https://api.idpay.ir/v1.1/payment",
-                                     headers={'Content-Type': 'application/json',
-                                              "X-API-KEY": 'c377d98e-0c65-4696-9d7f-db122b15b5e0',
-                                              'X-SANDBOX': "1"},
-                                     data=json.dumps({"order_id": order.id, "amount": int(order.get_total_cost) * 28000,
-                                                      "name": f"{costumer.user.phone}",
-                                                      "phone": f"{costumer.user.phone}",
-                                                      "mail": f"{costumer.user.email}",
-                                                      "desc": f"From Costumer : {costumer.id}",
-                                                      "callback": "http://185.235.41.190:8000/"})
-                                     ).json()
-                if "link" in resp.keys():
-                    Transactions.objects.create(
-                        order=order, transactionId=resp["id"], transactionLink=resp["link"])
-                    order.status = 3
-                    order.save()
-                    send_mail(
-                        'Congratulations !',
-                        f'Dear Admin , You Received A New Order From Customer : {costumer.user.username} ! With Transaction Id : {resp["id"]}',
-                        from_email=None,
-                        recipient_list=["mobinatashi2@gmail.com"],
-                        fail_silently=False,
-                    )
-                    return Response(resp["link"])
-                else:
-                    order.status = 0
-                    order.save()
-                    return Response({"Error": "Transaction Failed !"}, status=401)
-        else:
-            return Response({"Error": "At First Please Login/Select A valid Default Address :)"}, status=401)
+        account = Account.objects.get(user=request.user)
+        order = Order.objects.create(user=account)
+        srz_data = ItemSerializer(data=request.data, many=True)
+        if srz_data.is_valid():
+            srz_data.save()
+            return Response(srz_data.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
