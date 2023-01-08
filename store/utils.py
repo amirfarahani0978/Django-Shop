43e2a7ec48f4788a6c41f8ad96941e4c15fe43f8
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-08 03:47:08 +0330

    fix:fix my problem in use kavehnegarapi

--- store/utils.py
+++ store/utils.py
@@ -3,13 +3,13 @@ from django.conf import settings
 
 def send_otp_code(phone_number, code):
     try:
-        KavenegarAPI(settings.API_CODE)
+        kavenegar_instance = KavenegarAPI(settings.API_CODE)
         params = {
             'sender': '',
             'receptor': phone_number, 
             'message': f'{code}کد تایید شما ',
         }
-        response = settings.API_CODE.sms_send(params)
+        response = kavenegar_instance.sms_send(params)
         print(response)
     except APIException as e:
         print(e)
