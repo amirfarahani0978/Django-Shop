38d2c30762aab5222461a0c8ee3380691720b0a7
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-08 17:25:06 +0330

    feat:add and complete class verify for send otp code and delete it

--- store/account/api_views.py
+++ store/account/api_views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import RegisterSerializers, ProfileSerializer
+from .serializers import RegisterSerializers, VerifySerializers, ProfileSerializer
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .models import Account, OtpCode
@@ -31,8 +31,19 @@ class Register(APIView):
 class VerifyCodeView(APIView):
     def post(self, request):
         user_session = request.session['user_registration_info']
-        code_instance = OtpCode.objects.get(phone_number = user_session['phone_number'])
+        code_instance = OtpCode.objects.get(
+            phone_number=user_session['phone_number'])
         ser_data = VerifySerializers(data=request.POST)
+        if ser_data.is_valid():
+            cd = ser_data.validated_data
+            if code_instance.code == cd['code']:
+                Account.objects.create_user(
+                    phone_number=user_session['phone_number'], firstname=user_session['firstname'], lastname=user_session['lastname'], password=user_session['password'])
+                code_instance.delete()
+                return Response(ser_data.data, status=status.HTTP_201_CREATED)
+            return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 class ProfileUpdateView(APIView):
     permission_classes = [IsOwnerOrReadOnly,]
