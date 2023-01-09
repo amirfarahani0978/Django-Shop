dc5346b12eec8849e3e0a010bc687f839bf0e5bd
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-09 18:16:40 +0330

    fix:big problem about edit profile and permission and group and is_superuser exist in form

--- store/account/forms.py
+++ store/account/forms.py
@@ -48,7 +48,7 @@ class LoginForm(forms.Form):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Account
-        exclude = ('password','last_login','is_admin','is_active','phone_number',)
+        exclude = ('password','last_login','is_admin','is_active','is_superuser','phone_number','groups','user_permissions',)
 
 class VerfiyCodeForm(forms.Form):
     code = forms.IntegerField()
