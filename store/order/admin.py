219c19fb4f2fe4ef2bd0bc19e73461733affe0ab
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-01-09 10:35:34 +0330

    feat:add offer to admin panel

--- store/order/admin.py
+++ store/order/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import  Order, OrderItem
+from .models import  Order, OrderItem , Offer
 
+
+admin.site.register(Offer)
 
 class OrderItemAdmin(admin.TabularInline):
     model = OrderItem
