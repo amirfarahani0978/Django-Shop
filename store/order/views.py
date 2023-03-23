4bdd03cc50e36b94cc6ce8a1dc96c5b0cb3675bb
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-03-23 18:36:50 +0430

    feat:add orderpayview for pay the order

--- store/order/views.py
+++ store/order/views.py
@@ -10,6 +10,9 @@ import datetime
 from django.contrib import messages
 from product.models import Product
 from django.conf import settings
+import requests
+import json
+
 
 class CartView(View):
     def get(self, request):
@@ -92,6 +95,7 @@ class OfferApplyView(LoginRequiredMixin, View):
             order.save()
             return redirect('order:detail_order', order_id)
 
+
 if settings.SANDBOX:
     sandbox = 'sandbox'
 else:
@@ -100,6 +104,37 @@ else:
 ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
 ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
 ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
-class OrderPayView(LoginRequiredMixin , View):
-    def get(self , request , order_id):
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+CallbackURL = 'http://127.0.0.1:8080/verify/'
+
+
+class OrderPayView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
         order = Order.objects.get(id=order_id)
+        data = {
+            "MerchantID": settings.MERCHANT,
+            "Amount": order.get_total_price(),
+            "Description": description,
+            "Phone": requests.user.phone_number,
+            "CallbackURL": CallbackURL,
+        }
+        data = json.dumps(data)
+        # set content length by data
+        headers = {'content-type': 'application/json',
+                   'content-length': str(len(data))}
+        try:
+            response = requests.post(
+                ZP_API_REQUEST, data=data, headers=headers, timeout=10)
+
+            if response.status_code == 200:
+                response = response.json()
+                if response['Status'] == 100:
+                    return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']), 'authority': response['Authority']}
+                else:
+                    return {'status': False, 'code': str(response['Status'])}
+            return response
+
+        except requests.exceptions.Timeout:
+            return {'status': False, 'code': 'timeout'}
+        except requests.exceptions.ConnectionError:
+            return {'status': False, 'code': 'connection error'}
