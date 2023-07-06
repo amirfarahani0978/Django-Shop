0c3bf9c1022b22c8953502d4beb731ef1b8361d3
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-07-06 11:45:00 +0330

    feat:add get banner in views.py

--- store/home/views.py
+++ store/home/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.views import View
-from product.models import Product , Category
+from product.models import Product , Category 
+from .models import Baner
 from .forms import ContactForm
 
 class Home(View):
     def get(self, request , category_slug=None):
         products = Product.objects.filter(status_available=True)
+        baner = Baner.objects.all()
         categories = Category.objects.all()
         if category_slug:
             category = Category.objects.get(slug = category_slug)
             products = products.filter(category = category)
-        return render(request, 'base2.html', {'products': products , 'categories':categories})
+        return render(request, 'base2.html', {'products': products , 'categories':categories , 'baner':baner})
 
     # def post(self, request):
     #     return render(request, 'base.html')
