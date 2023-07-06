cc6121e7fbd30c279553412b88130b98104e2e95
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-07-06 11:56:27 +0330

    fix :change the name of banner to image in baner model

--- store/home/models.py
+++ store/home/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 from core.models import BaseModel
 class Baner(BaseModel):
-    banner = models.ImageField(upload_to ='banner/')
+    image = models.ImageField(upload_to ='banner/')
