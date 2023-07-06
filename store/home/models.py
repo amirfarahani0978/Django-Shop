4fcdd46c35e4e7c134cf1859f02845643089bc8d
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-07-06 12:47:37 +0330

    feat:add title to banner models

--- store/home/models.py
+++ store/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
 from core.models import BaseModel
 class Baner(BaseModel):
+    title = models.CharField(max_length=100)
     image = models.ImageField(upload_to ='banner/')
