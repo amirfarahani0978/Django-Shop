4d799ea1c14662b0759c98cad45ef10109dc84b0
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-07-22 16:58:50 +0330

    feat:add url for show about

--- store/home/urls.py
+++ store/home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home ,Contact
+from .views import Home ,Contact , AboutUsView
 app_name = 'home'
 urlpatterns =[
     path('' , Home.as_view() , name='home'),
     path('category/<slug:category_slug>/' , Home.as_view() , name='category_filter'),
-    path ('contact-us/' , Contact.as_view() , name='contact-us')
+    path ('contact-us/' , Contact.as_view() , name='contact-us'),
+    path ('about-us/' , AboutUsView.as_view() , name='about-us'),
 ]
