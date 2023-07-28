5c2dc8a8522d43191c601bdeb3cad173ed45e381
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-07-28 12:23:57 +0330

    fix:exist the problem about the write account.html in views.py for profileView

--- store/account/views.py
+++ store/account/views.py
@@ -114,4 +114,4 @@ class ProfileView(View):
     def get(self , request , user_id):
         user = Account.objects.get(id = user_id)
         order = Order.objects.filter(user = request.user)
-        return render(request , 'inc/acoount.html' , {'user':user ,'order':order})
+        return render(request , 'inc/account.html' , {'user':user ,'order':order})
