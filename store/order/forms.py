45a2816e654753de15b73caf3cca6f59fec098b0
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-08-07 14:56:55 +0330

    fix:fix the forms.py

--- store/order/forms.py
+++ store/order/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class CartAddForm(forms.Form):
-    quantity = forms.P(min_value=1 ,widget=forms.NumberInput(attrs={'class': 'form-control rounded'}))
+    quantity = forms.IntegerField(min_value=1 ,widget=forms.NumberInput(attrs={'class': 'form-control rounded'}))
 
 
 class OfferForm(forms.Form):
