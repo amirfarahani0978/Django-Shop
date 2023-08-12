4c4c8d3b655b0447b5fd0f501bd1031da4564660
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-08-12 16:32:21 +0330

    feat: add wishlist to admins.py

--- store/product/admin.py
+++ store/product/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Category ,Comment ,Product ,ProductFeature
+from .models import Category ,Comment ,Product ,ProductFeature , WishList
 
 admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(ProductFeature)
+admin.site.register(WishList)
 #admin.site.register(Product)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
