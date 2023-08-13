0d0cf8160c656ebf4d309b741e9f91197a9ddd33
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-08-13 09:14:20 +0330

    fix:fix the bug in url wishlist

--- store/product/urls.py
+++ store/product/urls.py
@@ -5,5 +5,5 @@ app_name='product'
 urlpatterns = [
     path('<slug:slug>/', ProductDetailView.as_view(), name='details'),
     path('api/' , include('product.api_urls' , namespace='api_product')),
-    path('addwisht/' , AddWishListView.as_view() , name = 'addwishlist'),
+    path('addwish/' , AddWishListView.as_view() , name = 'addwishlist'),
 ]
