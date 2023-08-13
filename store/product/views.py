b911d214f77dfd070a5012b252f88f263606d3c7
Amir <84338957+amirfarahani0978@users.noreply.github.com>
2023-08-13 09:04:17 +0330

    feat:complete the part of post method wishlist

--- store/product/views.py
+++ store/product/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 from django.shortcuts import get_object_or_404
-from .models import Product , Comment
+from .models import Product , Comment , WishList
 from order.forms import CartAddForm
+from django.http import JsonResponse
 # Create your views here.
 class ProductDetailView(View):
     def get(self, request, slug):
@@ -14,4 +15,12 @@ class ProductDetailView(View):
 
 class AddWishListView(View):
     def post(self , request):
-        ...
+        if request.is_ajax():
+            product_id = request.POST.get('product_id')
+            product = Product.objects.get(id=product_id)
+            user = self.request.user
+            wishlist, created = WishList.objects.get_or_create(user_id=user)
+            wishlist.product.add(product)
+            return JsonResponse({'message': 'Product added to wishlist!'})
+        else:
+            return JsonResponse({'error': 'Invalid request'}, status=400)
